f84d161b3742bafab6412987f479c8b628fed17d
Sofii2301 <sofialvarezrodero97@gmail.com>
2023-03-03 05:03:47 -0300

    incorporacion de herencia y archivos

--- app.py
+++ app.py
@@ -1,28 +1,31 @@
-from flask import Flask
+from flask import Flask, session, redirect, url_for
 from flask import render_template
 app = Flask(__name__)
 
-usuarios=[
-    {
-        "id" : "8",
-        "nombre" : "bruno"
-    },
-    {
-        "id" : "20",
-        "nombre" : "emanuel"
-    },
-    {
-        "id" : "10",
-        "nombre" : "sofi",
-    }
-]
-@app.route ("/")
-def index():
-    return "hola server"
-@app.route("/home")
+@app.route("/",methods=["GET"])
 def home(name='perfil'):
     return render_template('index.html',name=name)
-@app.route("/login")
-def login():
-    return render_template('login.html')
+
+@app.route("/ingresar")
+def ingresar():
+    return render_template('ingresar.html')
+
+@app.route("/directores")
+def directores():
+    return render_template('directores.html')
+
+@app.route("/generos")
+def generos():
+    return render_template('generos.html')
+
+@app.route("/agregarPeli")
+def agregar():
+    return render_template('agregarPeli.html')
+
+@app.route('/logout')
+def logout():
+  session.pop('username', None)
+  return redirect(url_for('index'))
+
+
 app.run( debug=True, port=8000 )

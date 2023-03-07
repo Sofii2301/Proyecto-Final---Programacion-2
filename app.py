52ad1d433318dd7ea58dec62fb4d1ecc00740691
Sofii2301 <sofialvarezrodero97@gmail.com>
2023-03-07 19:22:25 -0300

    agregarPeli sin terminar

--- app.py
+++ app.py
@@ -3,21 +3,30 @@ from flask import render_template
 import json
 app = Flask(__name__)
 app.secret_key = "secreto"
+
 #Archivos json
 with open("usuarios.json", encoding='utf-8') as users:
     usuarios_data = json.load(users)
-#archivos peliculas
 with open("peliculas.json", encoding='utf-8') as pelis:
     pelis_data = json.load(pelis)
+
+def usuario():
+    if 'user' in session:
+        name = session['user']
+    else:
+        name = ''
+    return name
+
+#Rutas
 @app.route("/",methods=["GET"])
-def home(nombre=''):
+def home():
     session['contador'] = session.get('contador',0) + 1
     contador_visitas=session['contador']
-    return render_template('index.html',name=nombre,peliculas=pelis_data,visitas=contador_visitas)
+    return render_template('index.html',name=usuario(),peliculas=pelis_data,visitas=contador_visitas)
 
 @app.route("/ingresar" )
 def ingresar():
-    return render_template('ingresar.html')
+    return render_template('ingresar.html',name=usuario())
 
 @app.route("/login", methods=["POST", "GET"])
 def form():
@@ -25,26 +34,18 @@ def form():
         for i in (usuarios_data):
             if i["nombre"]==request.form["username"] and i["contrasenia"] == request.form["password"]:
                 session["user"]=i["nombre"]
-                return redirect(url_for("log", name=session["user"]))
+                return redirect(url_for("home"))
         return "Usuario o contrasenia incorrectos"   
-    
-
-@app.route("/<name>")
-def log(name):
-    if 'user' in session:
-        return render_template("logueado.html", name=name)
-    else: 
-        return "Necesita estar logueado"
 
 @app.route("/directores")
 def directores():
-    return render_template('directores.html',peliculas=pelis_data)
+    return render_template('directores.html',name=usuario(),peliculas=pelis_data)
 @app.route("/generos")
 def generos():
-    return render_template('generos.html',peliculas=pelis_data,)
+    return render_template('generos.html',name=usuario(),peliculas=pelis_data,)
 @app.route("/imagenes")
 def imagenes():
-    return render_template('imagenes.html',peliculas=pelis_data)
+    return render_template('imagenes.html',name=usuario(),peliculas=pelis_data)
 @app.route("/confirmar", methods=["POST","GET"])
 def confirm():
     if request.method == "POST":
@@ -57,7 +58,7 @@ def confirm():
 @app.route("/buscarDirectores",methods=["POST","GET"])
 def buscar_directores():
     director1 = request.args.get('director')
-    return render_template('buscar_directores.html', peliculas=pelis_data, director1=director1)
+    return render_template('buscar_directores.html',name=usuario(), peliculas=pelis_data, director1=director1)
 @app.route("/confirmar_peliculas", methods=["POST","GET"])
 def confirm_peliculas():
     if request.method == "POST":
@@ -70,7 +71,7 @@ def confirm_peliculas():
 @app.route("/buscarPeliculas")
 def buscar_peliculas():
     titulo1=request.args.get('titulo')
-    return render_template('buscar_peliculas.html',peliculas=pelis_data,titulo1=titulo1)
+    return render_template('buscar_peliculas.html',name=usuario(),peliculas=pelis_data,titulo1=titulo1)
 @app.route("/confirmar_actores", methods=["POST","GET"])
 def confirm_actores():
     if request.method == "POST":
@@ -83,12 +84,39 @@ def confirm_actores():
 @app.route("/buscarActores")
 def buscar_actores():
     actor1=request.args.get('actor')
-    return render_template('buscar_actores.html',peliculas=pelis_data,actor1=actor1)
-@app.route("/agregarPeli")
+    return render_template('buscar_actores.html',name=usuario(),peliculas=pelis_data,actor1=actor1)
+@app.route("/agregarPeli", methods=['GET', 'POST'])
 def agregar():
-    return render_template('agregarPeli.html')
+    director=[]
+    genero=[]
+    if 'user' not in session: 
+        return redirect(url_for('home'))
+    if request.method == 'POST':
+        pelicula = {
+            "img": request.form['imagen'],
+            "Titulo": request.form['titulo'], 
+            "Director": request.form['director'], 
+            "año" : request.form['anio'], 
+            "Genero" : request.form['genero'],
+            "Sinopsis" : request.form['sinopsis'],
+            "Actores" :request.form['actores'],
+            "Comentarios":[
+                {
+                    "Usuario":session["user"],
+                    "Comentario":request.form["opinion"]
+                }
+            ]
+        }
+        
+        
+        pelis_data.append(pelicula)
+        
+    for datos in pelis_data:
+        director.append(datos['Director'])
+        genero.append(datos['Genero'])    
+    return render_template('agregarPeli.html',name=usuario(), directores=director, generos=genero)
 @app.route('/logout')
 def logout():
-  session.pop('username', None)
+  session.pop('user', None)
   return redirect(url_for('home'))
 app.run( debug=True, port=8000 )
